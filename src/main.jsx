ed6d5b0aac4aabdc20b5cdf2fdb3a9bb4fe574d6
Diego Sánchez <diegosanchezlondono@gmail.com>
2023-03-08 17:04:17 +0100

    se descargan mas dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
+//Importo BrowserRouter para envolver toda la aplicacion desde main.jsx 
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
